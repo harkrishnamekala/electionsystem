3fe646b1e6c234b33941779e31460ef97cada9bb
Harikrishna Mekala <haribabbu.krishna@gmail.com>
2018-01-08 16:50:12 +0530

    Added the Candidate Strucutre

--- SolidityCode/Election.sol
+++ SolidityCode/Election.sol
@@ -1,2 +1,30 @@
 pragma solidity ^0.4.19;
 
+contract Election {
+
+    struct candidate {
+        address addr;
+        bytes32 name;
+        bytes32 aadhar;
+        
+    }
+
+    mapping (address => uint256) public votesreceived;
+
+    bytes32[] public candidatelist;   
+
+    function Election(bytes32[] candidateNames) {
+        candidatelist = candidateNames;
+    }
+
+    function ValidateCandidate(bytes32 candidate) returns (bool) {
+        for(uint32 i=0;i<candidatelist.length;i++) { 
+            if (candidatelist[i] == candidate) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function
+}
